73bdf228171a550034ca267667ea9b0fd58f46e5
DillionApple <DillionMango@gmail.com>
2016-06-16 19:50:45 +0800

    add some getFromQuery

--- src/entity/Employee.java
+++ src/entity/Employee.java
@@ -1,5 +1,12 @@
 package entity;
 
+import database.MySQL;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /**
  * Created by Dillion on 6/7/16.
  */
@@ -24,12 +31,37 @@ public class Employee extends BaseEntity {
         this.setId(id);
     }
 
+    public static ArrayList<Employee> getEmployeeFromQuery(String sql) {
+        ArrayList<Employee> employeeArrayList = new ArrayList<Employee>();
+        ResultSet result;
+        try {
+            Statement statement = MySQL.getStatementInstance();
+            result = statement.executeQuery(sql);
+            while (result.next()) {
+                int id = result.getInt("id");
+                String name = result.getString("name");
+                Gender gender = Gender.valueOf(result.getString("gender"));
+                int age = result.getInt("age");
+                employeeArrayList.add(new Employee(id, name, gender, age));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return employeeArrayList;
+    }
+
     public void save() {
         super.save(String.format(SAVE_QUERY_TEMPLATE, TABLE_NAME, name, gender, age));
     }
 
     public static void main(String args[]) {
-        Employee employee = new Employee("wangsan", Gender.M, 20);
-        employee.save();
+        String sql = "select * from employee where id = 2";
+        ArrayList<Employee> employeeArrayList = Employee.getEmployeeFromQuery(sql);
+        for (Employee employee : employeeArrayList) {
+            System.out.print(employee.getId() + " ");
+            System.out.print(employee.name + " ");
+            System.out.print(employee.gender + " ");
+            System.out.println(employee.age);
+        }
     }
 }
