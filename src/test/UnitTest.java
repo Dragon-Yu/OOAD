8f2bbadb1a47111c18500f22ef36b3c729e602d0
dragon <xlyufudan@163.com>
2016-06-16 18:57:04 +0800

    UnitTest为正在写的单元测试 TestByDillion是原本的测试类

--- src/test/UnitTest.java
+++ src/test/UnitTest.java
@@ -1,19 +1,69 @@
 package test;
 
+import com.mysql.jdbc.authentication.MysqlClearPasswordPlugin;
+import database.MySQL;
 import entity.*;
-
 import java.sql.Date;
 import java.util.Random;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 /**
- * Created by Dillion on 6/9/16.
+ * Created by dragon on 16/6/15.
  */
-public class Test {
+public class UnitTest {
+    DeviceType deviceType1 = new DeviceType("#A1", "Haier TV", "Haier TV Set, 15");
+    Date date = new Date((new java.util.Date()).getTime());
+    //DeviceType deviceType2 = new DeviceType("#A2", "Haier Air Machine", "Haier Air Machine, 100W");
+//    @Test
+//    public void testCreateWorkDatabase(){
+//        MySQL.createWorkDatabase();
+//    }
+    @Test
+    public void testSavaDeviceType(){
+        System.out.println(deviceType1.exist());
+        assertEquals(false, deviceType1.exist());
+        deviceType1.save();
+        System.out.println(deviceType1.exist());
+        assertEquals(true, deviceType1.exist());
+    }
+    @Test
+    public void testAddPlan(){
+        deviceType1.addPlan(5, PlanName.SMALL, "something small");
+        deviceType1.addPlan(20, PlanName.MIDDLE, "something mid");
+        deviceType1.addPlan(30, PlanName.BIG, "something big");
+        //
+
+        assertEquals(true, deviceType1.exist());
+    }
+    @Test
+    public void testAddDevice(){
+        Device device1 = deviceType1.addDevice(date, "Cai Lun Road");
+        Device device2 = deviceType1.addDevice(date, "Zu Chong Zhi Road");
+        Device device3 = deviceType1.addDevice(date, "Zhang Heng Road");
+        System.out.println(device1.exist());
+        System.out.println(device2.exist());
+        System.out.println(device3.exist());
+        assertEquals(true, device1.exist());
+        assertEquals(true, device2.exist());
+        assertEquals(true, device3.exist());
+    }
+    @Test
+    public void testGeneratePlanSheets(){
+        for (Device device : deviceType1.getDevices()) {
+            device.generatePlanSheets();
+        }
+    }
+}
+
+class Readit {
     public static void main(String args[]) {
+
         DeviceType deviceType1 = new DeviceType("#A1", "Haier TV", "Haier TV Set, 15");
         System.out.println(deviceType1.exist());
         deviceType1.save();
-        System.out.println(deviceType1.exist());
+        System.out.println(deviceType1.exist());//
         deviceType1.addPlan(5, PlanName.SMALL, "something small");
         deviceType1.addPlan(20, PlanName.MIDDLE, "something mid");
         deviceType1.addPlan(30, PlanName.BIG, "something big");
